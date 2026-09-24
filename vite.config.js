6c6c8ba8d47917061092f828ed56f4409bcf6091
YulianaBorys <juna.9258@gmail.com>
2026-09-24 14:14:54 +0300

    fix docker

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5176,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
